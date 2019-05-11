dafe6a44925f20331e173e55fb144efb58c25333
Ronald Sims II <ron@orangemantis.net>
2019-05-10 20:28:11 -0400

    Started a photshop resizing script.

---
+++ create_ios_imageset.js
@@ -0,0 +1,3 @@
+//This script will create ios images for 120x120, 240x240 and 360x360 with the proper @1x, @2x, and @3x suffixes.
+var ogUnits = preferences.rulerUnits;
+prferences.rulerUnits = Units.PIXELS;
